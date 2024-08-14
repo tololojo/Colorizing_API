bd0f302072197fc4d235afa055bb7610d48f88f7
tololojo <loeru91@hotmail.com>
2024-08-14 18:05:27 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,5 @@
-from app import app as application
+from models_API.app.server import app as application
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(application, host="0.0.0.0", port=8080)
